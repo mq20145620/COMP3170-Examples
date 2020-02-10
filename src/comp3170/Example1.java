5a73d35f70bd7b5f84c9d2636cd0141d943cafb8
Malcolm Ryan <malcolm.ryan@gmail.com>
2020-02-10 12:55:12 +1100

    Adding boilerplate

--- src/comp3170/Example1.java
+++ src/comp3170/Example1.java
@@ -1,11 +1,17 @@
 package comp3170;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GL2ES1;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.awt.GLCanvas;
-
+import com.jogamp.opengl.fixedfunc.GLLightingFunc;
 
 public class Example1 extends JFrame implements GLEventListener {
 
@@ -20,29 +26,34 @@ public class Example1 extends JFrame implements GLEventListener {
 		
 		this.add(myCanvas);
 		this.setVisible(true);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 	}
 
 	@Override
 	public void init(GLAutoDrawable drawable) {
-		// TODO Auto-generated method stub
+		GL2 gl = drawable.getGL().getGL2();		
+		gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		
 	}
 
 	@Override
 	public void dispose(GLAutoDrawable drawable) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void display(GLAutoDrawable drawable) {
-		// TODO Auto-generated method stub
-		
+		final GL2 gl = drawable.getGL().getGL2();
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT);		
 	}
 
 	@Override
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-		// TODO Auto-generated method stub
+		GL2 gl = drawable.getGL().getGL2();
+		final float aspect = (float) width / (float) height;
 		
 	}
 
